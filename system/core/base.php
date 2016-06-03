796335db96504d73d4d6ece0f41cafe55ad42fbc
davidlu <davidlu8@qq.com>
2016-06-03 14:58:44 +0800

    flight 0.1

---
+++ system/core/base.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: David
+ * Date: 2016/6/3
+ * Time: 14:58
+ */
--- system/core/initialize.php
+++ system/core/initialize.php
@@ -1,1 +1,17 @@
 <?php
+class base {
+    public static function autoRequire($dir) {
+        if (is_dir($dir)) {
+            if ($handle = opendir($dir)) {
+                while (($file = readdir($handle)) !== false) {
+                    if (filetype($dir.$file) == 'file') {
+                        require_once($dir.$file);
+                    }
+                }
+                closedir($handle);
+            }
+        } else {
+            die('此目录不存在');
+        }
+    }
+}
