fbb96c8abf83c23438cc94e8fc234be6b95e336c
davidlu <davidlu8@qq.com>
2016-06-03 14:56:36 +0800

    flight 0.1

--- index.php
+++ index.php
@@ -3,3 +3,7 @@
 echo '<pre>';
 print_r($_GET);
 print_r($_SERVER['REQUEST_URI']);
+define('SYSTEMPATH', 'system'.DIRECTORY_SEPARATOR);
+define('APPPATH', 'application'.DIRECTORY_SEPARATOR);
+
+require_once SYSTEMPATH.'core/initialize.php';
