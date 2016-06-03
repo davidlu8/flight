d18cd4c3465c1cbf1ce551fc353f621ab7d341ba
davidlu <davidlu8@qq.com>
2016-06-03 15:27:06 +0800

    flight 0.1

---
+++ system/core/uri.php
@@ -0,0 +1,18 @@
+<?php
+
+class uri {
+    public $url;
+    public $segments = array();
+    function __construct() {
+        $this->url = substr($_SERVER['REQUEST_URI'], 1);
+        $this->segments = explode('/', $this->url);
+    }
+
+    public function item($index) {
+        if (isset($this->segments[$index])) {
+            return $this->segments[$index];
+        } else {
+            return '';
+        }
+    }
+}
