2e008ddef625551a027a5aef0233894f0e06037f
davidlu <davidlu8@qq.com>
2016-06-03 17:21:53 +0800

    flight 0.1

--- system/core/application.php
+++ system/core/application.php
@@ -1,6 +1,8 @@
 <?php
 class application {
     public function run() {
-
+        Fl::logger()->debug("--------------");
+        Fl::logger()->debug("[ Service = " . $this->_service." ] [ IP = ".common::getIp()." ] ", 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']);
+        Fl::logger()->debug(" Agent : ", $_SERVER['HTTP_USER_AGENT']);
     }
 }
