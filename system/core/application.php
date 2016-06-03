ce3f63dbad8694d339faab104ea8ade1d848fcfd
davidlu <davidlu8@qq.com>
2016-06-03 17:49:01 +0800

    flight 0.1

--- system/core/application.php
+++ system/core/application.php
@@ -1,9 +1,9 @@
 <?php
 class application {
     public function run() {
-        $service = sprintf('%s.%s', FL::uri()->segments[0], FL::uri()->segments[1]);
+        $controller = sprintf('%s.%s', FL::uri()->segments[0], FL::uri()->segments[1]);
         Fl::logger()->debug("--------------");
-        Fl::logger()->debug("[ Service = " .$service." ] [ IP = ".common::getIp()." ] ", 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']);
+        Fl::logger()->debug("[ Controller = " .$controller." ] [ IP = ".common::getIp()." ] ", 'http://'.$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'].'?'.$_SERVER['QUERY_STRING']);
         Fl::logger()->debug(" Agent : ", $_SERVER['HTTP_USER_AGENT']);
     }
 }
