a23b97ca18650a5e097e64f9f1c6b09006dd2964
davidlu <davidlu8@qq.com>
2016-06-06 12:50:39 +0800

    flight 0.1

---
+++ system/libraries/load.php
@@ -0,0 +1,13 @@
+<?php
+class Load {
+    public static function model($name) {
+        $name = strtolower($name);
+        $modelFilePath = sprintf('%smodel/%s.model.php', APPPATH, $name);
+        if (!file_exists($modelFilePath)) {
+            die("Control class does not exist : ".$modelFilePath);
+        }
+        require_once($modelFilePath);
+        $modelName = sprintf('%sModel', ucfirst($name));
+        return new $modelName();
+    }
+}
