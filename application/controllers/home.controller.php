401c69baa5329518b1ee2343fcadbccdc4e48a2d
davidlu <davidlu8@qq.com>
2016-06-06 22:49:42 +0800

    flight 0.1

--- application/controllers/home.controller.php
+++ application/controllers/home.controller.php
@@ -13,7 +13,7 @@ class homeControl extends baseControl {
                 'type' => 0,
             );
             $data = http_build_query($param);
-            fputs($fp, 'POST /api.php HTTP/1.1\r\n');
+            fputs($fp, 'POST /tv HTTP/1.1\r\n');
             fputs($fp, 'Host: www.yuai6.com\r\n');
             fputs($fp, 'Content-length: '.strlen($data).'\r\n');
             fputs($fp, 'Connection: Close\r\n\r\n');
