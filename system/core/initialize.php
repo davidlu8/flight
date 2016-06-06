f0741b3f45bc32c3117867f67f1545342f491659
davidlu <davidlu8@qq.com>
2016-06-06 10:36:11 +0800

    flight 0.1

--- system/core/initialize.php
+++ system/core/initialize.php
@@ -4,6 +4,9 @@ require_once SYSTEMPATH.'core/base.php';
 base::autoRequire(SYSTEMPATH.'core', array('initialize.php', 'base.php'));
 base::autoRequire(SYSTEMPATH.'libraries');
 
+
+require_once APPPATH.'controllers/base.controller.php';
+
 echo '<pre>';
 print_r(FL::uri());
 
