c3607ee4e66da6d49b521a2a61fb1dd955fd6c43
davidlu <davidlu8@qq.com>
2016-06-06 22:27:31 +0800

    flight 0.1

--- index.php
+++ index.php
@@ -3,6 +3,3 @@ define('SYSTEMPATH', 'system'.DIRECTORY_SEPARATOR);
 define('APPPATH', 'application'.DIRECTORY_SEPARATOR);
 
 require_once SYSTEMPATH.'core/initialize.php';
-
-echo '<pre>';
-print_r((array)'123');
