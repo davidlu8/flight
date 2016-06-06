fcf0a44448e4b89f1afbff3eb44bdce8052d92fa
davidlu <davidlu8@qq.com>
2016-06-06 22:39:05 +0800

    flight 0.1

--- application/controllers/home.controller.php
+++ application/controllers/home.controller.php
@@ -5,7 +5,23 @@ class homeControl extends baseControl {
     }
 
     public function index() {
-        $userinfoDal = Load::model('userinfo');
+        $fp = fsockopen('api.yuai6.com', 80, $errno, $errstr, 30);
+        if (!$fp) {
+            fputs($fp, 'POST /api.php HTTP/1.1\r\n');
+            fputs($fp, 'Host: api.yuai.com\r\n');
+            fputs($fp, 'Connection: Close\r\n\r\n');
+            fputs($fp, 'service=user.info&id=1000070&type=0');
+            $response = '';
+            while($row=fread($fp, 4096)){
+                $response .= $row;
+            }
+
+            fclose($fp);
+
+            echo $response;
+        } else {
+            die('The host can not open');
+        }
 
     }
 }
