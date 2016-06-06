efeaf6b55d1b4c77ea79d4f98fd9704fcb84495f
davidlu <davidlu8@qq.com>
2016-06-06 12:49:59 +0800

    flight 0.1

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-
-echo '<pre>';
-print_r($_GET);
-print_r($_SERVER['REQUEST_URI']);
 define('SYSTEMPATH', 'system'.DIRECTORY_SEPARATOR);
 define('APPPATH', 'application'.DIRECTORY_SEPARATOR);
 
