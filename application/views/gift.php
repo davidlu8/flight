51375c8be395b6beeb965c7d1b0cd77b15cbf8c2
davidlu <davidlu8@qq.com>
2016-06-06 13:17:14 +0800

    flight 0.1

--- application/views/gift.php
+++ application/views/gift.php
@@ -12,8 +12,9 @@
 </head>
 <body>
 </body>
-<?php echo date('H:i', strtotime($item['GIFTHISTORY_ADD_TIME'])) ?>
-
-<a href="<?php echo $ownerInfo['USERINFO_USER_ID'] ?>" class="text-danger"><?php echo $ownerInfo['USERINFO_NICKNAME'] ?></a>送给了<a href="<?php echo $userInfo['USERINFO_USER_ID'] ?>" class="text-danger"><?php echo $userInfo['USERINFO_NICKNAME'] ?></a><?php echo $item['GIFTHISTORY_GIFT_AMOUNT'] ?>个<?php echo $gift['GIFT_NAME'] ?>,
-掌声响起来...
+<div class="wrapper">
+    <?php echo date('H:i', strtotime($item['GIFTHISTORY_ADD_TIME'])) ?>
+    <a href="<?php echo $ownerInfo['USERINFO_USER_ID'] ?>" class="text-danger"><?php echo $ownerInfo['USERINFO_NICKNAME'] ?></a>送给了<a href="<?php echo $userInfo['USERINFO_USER_ID'] ?>" class="text-danger"><?php echo $userInfo['USERINFO_NICKNAME'] ?></a><?php echo $item['GIFTHISTORY_GIFT_AMOUNT'] ?>个<?php echo $gift['GIFT_NAME'] ?>,
+    掌声响起来...
+</div>
 </html>
