7929c28cd51a098d454124a411830ea0959cc285
davidlu <davidlu8@qq.com>
2016-06-06 22:56:56 +0800

    flight 0.1

--- application/controllers/home.controller.php
+++ application/controllers/home.controller.php
@@ -15,10 +15,7 @@ class homeControl extends baseControl {
             $data = http_build_query($param);
             fputs($fp, 'GET / HTTP/1.0\r\n');
             fputs($fp, 'Host: www.warmvoice.cn\r\n');
-            fputs($fp, 'Content-type: application/x-www-form-urlencoded\r\n');
-            fputs($fp, 'Content-length: '.strlen($data).'\r\n');
             fputs($fp, 'Connection: Close\r\n\r\n');
-            fputs($fp, $data);
             $response = '';
             while($row=fread($fp, 4096)){
                 $response .= $row;
