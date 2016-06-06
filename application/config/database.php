465805c51e8e251319395312f785bb1a84d01ff3
davidlu <davidlu8@qq.com>
2016-06-06 12:10:57 +0800

    flight 0.1

---
+++ application/config/database.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    0 => array(
+        'autoload' => true,
+        'hostname' => 'yuaiappdb.mysql.rds.aliyuncs.com',
+        'hostport' => 3306,
+        'username' => 'yuai',
+        'password' => 'Wodemima88',
+        'database' => 'yuai',
+    )
+);
