16002359d0301d634e01756e28b7a1e0e7c2a8a7
davidlu <davidlu8@qq.com>
2016-06-06 12:54:15 +0800

    flight 0.1

--- application/controllers/tv.controller.php
+++ application/controllers/tv.controller.php
@@ -1,11 +1,11 @@
 <?php
-class homeControl extends baseControl {
+class tvControl extends baseControl {
     public function __construct() {
         parent::__construct();
     }
 
     public function index() {
         $userinfoDal = Load::model('userinfo');
-
+        
     }
 }
