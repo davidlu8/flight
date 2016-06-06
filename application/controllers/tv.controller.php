e3effffbd0702731ec2485f185d93db20797b052
davidlu <davidlu8@qq.com>
2016-06-06 13:16:11 +0800

    flight 0.1

--- application/controllers/tv.controller.php
+++ application/controllers/tv.controller.php
@@ -7,6 +7,7 @@ class tvControl extends baseControl {
     public function index() {
         $userinfoDal = Load::model('userinfo');
         $gifthistoryDal = Load::model('gifthistory');
+        $giftDal = Load::model('gift');
         $filterData = array(
             'order' => array(
                 'GIFTHISTORY_ADD_TIME' => 'desc',
@@ -16,7 +17,7 @@ class tvControl extends baseControl {
 
         $ownerInfo = $userinfoDal->find($item['GIFTHISTORY_OWNER_ID']);
         $userInfo = $userinfoDal->find($item['GIFTHISTORY_USER_ID']);
-        $gift = $gifthistoryDal->find($item['GIFTHISTORY_GIFT_ID']);
+        $gift = $giftDal->find($item['GIFTHISTORY_GIFT_ID']);
         echo '<pre>';
         print_r($gift);
 
