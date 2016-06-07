786ae248e323aba55a81e7d65f8341ef5f7627b1
davidlu <davidlu8@qq.com>
2016-06-07 17:15:29 +0800

    flight 0.1

--- application/controllers/home.controller.php
+++ application/controllers/home.controller.php
@@ -5,7 +5,7 @@ class homeControl extends baseControl {
     }
 
     public function index() {
-        $fp = fsockopen('www.yuai6.com', 80, $errno, $errstr, 30);
+        $fp = fsockopen('site.yuai6.com', 80, $errno, $errstr, 30);
         if ($fp) {
             $param = array(
                 'service' => 'user.info',
@@ -14,7 +14,7 @@ class homeControl extends baseControl {
             );
             $data = http_build_query($param);
             fputs($fp, "POST /home/test HTTP/1.1\r\n");
-            fputs($fp, "Host: www.yuai6.com\r\n");
+            fputs($fp, "Host: site.yuai6.com\r\n");
             fputs($fp, "Content-type: application/x-www-form-urlencoded\r\n");
             fputs($fp, "Content-length: ".strlen($data)."\r\n");
             fputs($fp, "Cookie: cur_query=you&me\r\n");
