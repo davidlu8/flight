a1bf0b0cb79010a89ad489351406d51509fc16b2
davidlu <davidlu8@qq.com>
2016-06-10 22:45:35 +0800

    flight 0.1

--- application/controllers/mall.controller.php
+++ application/controllers/mall.controller.php
@@ -70,11 +70,6 @@ class mallControl extends baseControl {
         $userattr = $userattrDal->find($id);
         $commodityDal = Load::model('commodity');
         $commodity = $commodityDal->find($commodityID);
-        echo DB::lastQuery();
-
-        echo '<pre>';
-        print_r($commodity);
-
         if ($userattr['USERATTR_CREDIT'] < $commodity['COMMODITY_PRICE']) {
             $data = array(
                 'errCode' => 1,
