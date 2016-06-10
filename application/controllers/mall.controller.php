4017dc0409511f67148c06c59c85e3a2fab44764
davidlu <davidlu8@qq.com>
2016-06-10 15:51:00 +0800

    flight 0.1

--- application/controllers/mall.controller.php
+++ application/controllers/mall.controller.php
@@ -5,6 +5,7 @@ class mallControl extends baseControl {
     }
 
     public function index() {
+        echo phpinfo();
         FL::session()->set('id', 1);
         $id = FL::input()->get('id', 0);
         $timestamp = FL::input()->get('timestamp', 0);
