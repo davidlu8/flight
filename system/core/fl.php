6aadce5b3d3cb0b725090faf3925741fbf3ad0b5
davidlu <davidlu8@qq.com>
2016-06-10 13:47:40 +0800

    flight 0.1

--- system/core/fl.php
+++ system/core/fl.php
@@ -5,9 +5,26 @@ class FL {
     public static function __callStatic($funcname, $arguments) {
         if (class_exists($funcname)) {
             if (!isset(self::$instance[$funcname]) || !(self::$instance[$funcname] instanceof $funcname)) {
-                echo '<pre>';
-                print_r($arguments);
-                self::$instance[$funcname] = new $funcname($arguments);
+                switch(count($arguments)) {
+                    case 0:
+                        self::$instance[$funcname] = new $funcname();
+                        break;
+                    case 1:
+                        self::$instance[$funcname] = new $funcname($arguments[0]);
+                        break;
+                    case 2:
+                        self::$instance[$funcname] = new $funcname($arguments[0], $arguments[1]);
+                        break;
+                    case 3:
+                        self::$instance[$funcname] = new $funcname($arguments[0], $arguments[1], $arguments[2]);
+                        break;
+                    case 4:
+                        self::$instance[$funcname] = new $funcname($arguments[0], $arguments[1], $arguments[2], $arguments[3]);
+                        break;
+                    case 5:
+                        self::$instance[$funcname] = new $funcname($arguments[0], $arguments[1], $arguments[2], $arguments[3], $arguments[4]);
+                        break;
+                }
             }
             return self::$instance[$funcname];
         } else {
