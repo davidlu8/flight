4bfd57dc9053d68cd4ca20fff869313ab7c7c10e
davidlu <davidlu8@qq.com>
2016-06-10 22:43:21 +0800

    flight 0.1

--- application/controllers/mall.controller.php
+++ application/controllers/mall.controller.php
@@ -71,9 +71,10 @@ class mallControl extends baseControl {
         $commodityDal = Load::model('commodity');
         $commodity = $commodityDal->find($commodityID);
 
+        echo '<pre>';
+        print_r($userattr);
+        print_r($commodity);
         if ($userattr['USERATTR_CREDIT'] < $commodity['COMMODITY_PRICE']) {
-            echo '<pre>';
-            print_r($userattr);
             $data = array(
                 'errCode' => 1,
                 'errMsg' => '你的积分不足'
