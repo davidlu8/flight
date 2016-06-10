161221c644af8209db49f8b42b86524b4722dec4
davidlu <davidlu8@qq.com>
2016-06-10 21:41:45 +0800

    flight 0.1

--- application/views/mall_item.php
+++ application/views/mall_item.php
@@ -19,7 +19,7 @@
         <?php echo $item['COMMODITY_NAME'] ?>
     </div>
     <div class="price">
-        <?php echo $item['COMMODITY_COUNT'] ?>人已兑换
+        <?php echo $item['COMMODITY_COUNT'] ?>人已兑换<br/>
         <?php echo number_format($item['COMMODITY_COUNT'], 0) ?>积分
         <div class="exchange">
             <a href="#" class="btn btn-large btn-block btn-danger">兑换</a>
