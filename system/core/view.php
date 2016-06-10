c0550981514dbf2c4b0dddc7e5a1ff1e1650c6bb
davidlu <davidlu8@qq.com>
2016-06-10 13:31:21 +0800

    flight 0.1

---
+++ system/core/view.php
@@ -0,0 +1,16 @@
+<?php
+class view {
+    public $viewFolder = APPPATH.'views/';
+    function __construct($viewName, $data) {
+        $viewPath = $this->viewFolder.$viewName.'.php';
+        if (file_exists($viewPath)) {
+            if (count($data) > 0) {
+                extract($data);
+                include($viewPath);
+            }
+        } else {
+            die('The file of template does not exist!');
+        }
+    }
+
+}
