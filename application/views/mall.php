c408c18b55c80967bdb4fd3bf5f39cfaa9002da0
davidlu <davidlu8@qq.com>
2016-06-10 13:57:44 +0800

    flight 0.1

--- application/views/mall.php
+++ application/views/mall.php
@@ -14,8 +14,8 @@
 </body>
 <header>
     <div class="row">
-        <div class="col-md-6">.col-md-8</div>
-        <div class="col-md-2 text-right">.col-md-4</div>
+        <div class="col-md-6">.col-md-6</div>
+        <div class="col-md-2 text-right">.col-md-2</div>
     </div>
 </header>
 <div class="mall-wrapper">
