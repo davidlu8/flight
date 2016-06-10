946b8e99af04e7e8851f04bc807ea8916a749a10
davidlu <davidlu8@qq.com>
2016-06-10 12:11:54 +0800

    flight 0.1

--- system/core/uri.php
+++ system/core/uri.php
@@ -1,13 +1,18 @@
 <?php
 class uri {
     public $url;
+    public $path;
+    public $query;
     public $segments = array();
     function __construct() {
         $this->url = substr($_SERVER['REQUEST_URI'], 1);
+        $urlInfo = parse_url($this->url);
+        $this->path = $urlInfo['path'];
+        $this->query = $urlInfo['query'];
 
         echo '<pre>';
         print_r(parse_url($this->url));
-        $this->segments = preg_grep('/[\w]+/', explode('/', $this->url));
+        $this->segments = preg_grep('/[\w]+/', explode('/', $this->path));
         if (!isset($this->segments[0]) || empty($this->segments[0])) {
             $this->segments[0] = 'home';
         }
