4ab23e944ab186ec2bacc754a4013152d64e9032
davidlu <davidlu8@qq.com>
2016-06-10 13:38:42 +0800

    flight 0.1

--- system/core/fl.php
+++ system/core/fl.php
@@ -5,6 +5,8 @@ class FL {
     public static function __callStatic($funcname, $arguments) {
         if (class_exists($funcname)) {
             if (!isset(self::$instance[$funcname]) || !(self::$instance[$funcname] instanceof $funcname)) {
+                echo '<pre>';
+                print_r($arguments);
                 self::$instance[$funcname] = new $funcname($arguments);
             }
             return self::$instance[$funcname];
