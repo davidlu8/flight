808bf54104725b6e86b9e3477de2ac8faa0feaec
davidlu <davidlu8@qq.com>
2016-06-10 13:48:03 +0800

    flight 0.1

---
+++ application/views/mall.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <title>电视滚动屏</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="stylesheet" href="/packages/flatui/css/vendor/bootstrap.min.css">
+    <link rel="stylesheet" href="/packages/flatui/css/flat-ui-pro.min.css">
+    <link rel="stylesheet" href="/page/theme/styles/styles.css">
+    <script type="text/javascript" charset="utf-8" src="/packages/jquery/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" charset="utf-8" src="/page/theme/scripts/script.js"></script>
+</head>
+<body>
+</body>
+<div class="wrapper">
+    <?php echo date('H:i', strtotime($item['GIFTHISTORY_ADD_TIME'])) ?>
+    <a href="userID:<?php echo $ownerInfo['USERINFO_USER_ID'] ?>" class="text-danger"><?php echo $ownerInfo['USERINFO_NICKNAME'] ?></a>送给了<a href="userID:<?php echo $userInfo['USERINFO_USER_ID'] ?>" class="text-danger"><?php echo $userInfo['USERINFO_NICKNAME'] ?></a><?php echo $item['GIFTHISTORY_GIFT_AMOUNT'] ?>个<span class="text-success">[<?php echo $gift['GIFT_NAME'] ?> <img src="<?php echo $gift['GIFT_IMAGE'] ?>" width="28" />]</span>,
+    掌声响起来...<a href="/page/tv.html" class="text-default title-tv"><img src="/page/theme/images/tv.jpg" width="30"/> 我要上电视</a>
+</div>
+</html>
