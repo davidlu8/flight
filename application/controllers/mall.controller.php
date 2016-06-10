6a390c1ae68ad351508d45a12549c3f6415a96fb
davidlu <davidlu8@qq.com>
2016-06-10 11:09:22 +0800

    flight 0.1

---
+++ application/controllers/mall.controller.php
@@ -0,0 +1,24 @@
+<?php
+class mallControl extends baseControl {
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        $userinfoDal = Load::model('userinfo');
+        $gifthistoryDal = Load::model('gifthistory');
+        $giftDal = Load::model('gift');
+        $filterData = array(
+            'order' => array(
+                'GIFTHISTORY_ADD_TIME' => 'desc',
+            )
+        );
+        $item = $gifthistoryDal->item($filterData);
+
+        $ownerInfo = $userinfoDal->find($item['GIFTHISTORY_OWNER_ID']);
+        $userInfo = $userinfoDal->find($item['GIFTHISTORY_USER_ID']);
+        $gift = $giftDal->find($item['GIFTHISTORY_GIFT_ID']);
+
+        require APPPATH.'views/gift.php';
+    }
+}
