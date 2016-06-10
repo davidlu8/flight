b4adecb040e2c798b3d877d689222a7d54db67c1
davidlu <davidlu8@qq.com>
2016-06-10 23:17:21 +0800

    flight 0.1

--- application/views/mall_confirm.php
+++ application/views/mall_confirm.php
@@ -31,7 +31,7 @@
     <?php endif ?>
 </div>
 <div class="desc-wrapper">
-    <button class="btn btn-large btn-block btn-danger">确认兑换</button>
+    <button class="btn btn-large btn-block btn-danger" name="exchange">确认兑换</button>
 </div>
 </form>
 </body>
