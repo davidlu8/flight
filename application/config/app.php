2b79420abc317fdb10239522d2ecd647be95edbf
davidlu <davidlu8@qq.com>
2016-06-11 22:40:48 +0800

    flight 0.1

---
+++ application/config/app.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'vipvalue' => array(
+        0 => array(1, 3),
+        1 => array(1, 30),
+        2 => array(2, 100),
+        3 => array(3, 360),
+        4 => array(4, 1080),
+    )
+];
--- application/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: David
- * Date: 2016/6/3
- * Time: 18:07
- */
