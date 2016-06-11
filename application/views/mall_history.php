14e27ab631dfe6a3209e20185ccc69876c8331d8
davidlu <davidlu8@qq.com>
2016-06-11 23:21:05 +0800

    flight 0.1

--- application/views/mall_history.php
+++ application/views/mall_history.php
@@ -25,7 +25,7 @@
     <li>
         兑换<span class="text-danger">[<?php echo $item['COMMODITY_NAME'] ?>]</span>商品
         <div class="date">
-            <?php echo date('m/d', strtotime($item['EXCHANGE_ADD_TIME'])) ?>
+            <?php echo date('m/d H:i', strtotime($item['EXCHANGE_ADD_TIME'])) ?>
         </div>
     </li>
     <?php endforeach ?>
