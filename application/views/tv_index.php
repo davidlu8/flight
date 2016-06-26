41edec98aa5ab0965b39fd64184f9ea275031f00
davidlu <davidlu8@qq.com>
2016-06-26 21:02:03 +0800

    flight 0.1

--- application/views/tv_index.php
+++ application/views/tv_index.php
@@ -14,8 +14,7 @@
 </body>
 <div class="wrapper">
     <?php echo date('H:i', strtotime($item['GIFTHISTORY_ADD_TIME'])) ?>
-    <?php echo '<pre>';print_r($item);print_r($ownerInfo) ?>
-    <?php if ($item['USERATTR_TV'] == 1): ?>111<span class="text-danger">神秘人</span><?php else: ?>222<a href="userID:<?php echo $ownerInfo['USERINFO_USER_ID'] ?>" class="text-danger"><?php echo $ownerInfo['USERINFO_NICKNAME'] ?></a><?php endif ?>送给了<a href="userID:<?php echo $userInfo['USERINFO_USER_ID'] ?>" class="text-danger"><?php echo $userInfo['USERINFO_NICKNAME'] ?></a><?php echo $item['GIFTHISTORY_GIFT_AMOUNT'] ?>个<span class="text-success">[<?php echo $gift['GIFT_NAME'] ?> <img src="<?php echo $gift['GIFT_IMAGE'] ?>" width="28" />]</span>,
+    <?php if ($item['USERATTR_TV'] == 1): ?><span class="text-danger">神秘人</span><?php else: ?><a href="userID:<?php echo $ownerInfo['USERINFO_USER_ID'] ?>" class="text-danger"><?php echo $ownerInfo['USERINFO_NICKNAME'] ?></a><?php endif ?>送给了<a href="userID:<?php echo $userInfo['USERINFO_USER_ID'] ?>" class="text-danger"><?php echo $userInfo['USERINFO_NICKNAME'] ?></a><?php echo $item['GIFTHISTORY_GIFT_AMOUNT'] ?>个<span class="text-success">[<?php echo $gift['GIFT_NAME'] ?> <img src="<?php echo $gift['GIFT_IMAGE'] ?>" width="28" />]</span>,
     掌声响起来...<a href="/tv/help" class="text-default title-tv"><img src="/page/theme/images/tv.png" width="20" height="20" /> 我要上电视</a>
 </div>
 </html>
