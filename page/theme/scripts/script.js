9f98ff557c66fb9d5bb89162d045c0508a7db468
davidlu <davidlu8@qq.com>
2016-06-26 21:17:26 +0800

    flight 0.1

--- page/theme/scripts/script.js
+++ page/theme/scripts/script.js
@@ -35,7 +35,6 @@ $(document).ready(function(){
 
     $('input[name=tv_setting]').click(function() {
         var value = $(this).attr("value");
-        alert(value);
         $.ajax({
             url: '/tv/settings',
             type: "post",
