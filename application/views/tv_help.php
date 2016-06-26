38816a056f58bb5915f6d768d3e09526f709414b
davidlu <davidlu8@qq.com>
2016-06-26 14:50:13 +0800

    flight 0.1

--- application/views/tv_help.php
+++ application/views/tv_help.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <link rel="stylesheet" href="/packages/flatui/css/vendor/bootstrap.min.css">
     <link rel="stylesheet" href="/packages/flatui/css/flat-ui-pro.min.css">
-    <link rel="stylesheet" href="theme/styles/styles.css">
+    <link rel="stylesheet" href="/page/theme/styles/styles.css">
     <script type="text/javascript" charset="utf-8" src="/packages/jquery/jquery-1.11.3.min.js"></script>
 </head>
 <body>
